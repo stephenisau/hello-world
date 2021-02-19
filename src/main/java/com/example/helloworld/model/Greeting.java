33d05f761ce66bb9635222c185ec644a8afb6fe2
stephenisau <sfcstau@gmail.com>
2021-02-19 11:59:06 -0800

    Add Greeting model

--- src/main/java/com/example/helloworld/model/Greeting.java
+++ src/main/java/com/example/helloworld/model/Greeting.java
@@ -6,15 +6,28 @@ public class Greeting {
     private final UUID id;
     private final String content;
 
+    /**
+     * Greeting constructor
+     * @param id
+     * @param content
+     */
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
 
+    /**
+     * Getter for id 
+     * @return
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * Getter for content
+     * @return
+     */
     public String getContent() {
         return content;
     }
