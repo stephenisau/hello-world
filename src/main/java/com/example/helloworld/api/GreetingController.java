b4c3119deb90c7a85ad348476a695f83449fad6c
stephenisau <sfcstau@gmail.com>
2021-02-19 12:30:49 -0800

    Create data access object on greetingDao

--- src/main/java/com/example/helloworld/api/GreetingController.java
+++ src/main/java/com/example/helloworld/api/GreetingController.java
@@ -1,13 +1,33 @@
 package com.example.helloworld.api;
 
+import com.example.helloworld.model.Greeting;
+import com.example.helloworld.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
 public class GreetingController {
     private final GreetingService greetingService;
 
+    /**
+     * Set our greetingService instance to be the greetingService.
+     * @Autowired for dependency injection
+     * @param greetingService
+     */
+    @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public void addGreeting(Greeting greeting) {
+
+    /**
+     * Add greeting instance to our greeting service.
+     * Implements addGreeting method on our greetingService class
+     * @param greeting
+     */
+    @PostMapping("/greeting")
+    public void addGreeting(@RequestBody Greeting greeting) {
         greetingService.addGreeting(greeting);
     }
 }
